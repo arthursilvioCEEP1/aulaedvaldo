adbb046fb2a6a3819a955ae4c6c2d9f67be7aba0
Arthur Vitor <127210318+arthursilvioCEEP1@users.noreply.github.com>
2023-06-22 15:05:23 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -8,17 +8,27 @@ $dbname ="empregados";
 
 $conexao = mysqli_connect($hostname, $username, $password, $dbname) or die("Não foi possível fazer a conexão ao banco de dados.");
 
-//mysqli_select_db($dbnome)
+?>
+    <h1><bold>Edvaldo Aula PHP</bold></h1>
+    <form method="GET">
+        <label for="nome">Nome</label><br>
+        <input type="text" id="nome" placeholder="Digite o nome aqui">
+        <input type="submit" value = "salvar">
+    </form>
+<?php
+
+//aqui vou salvar dados na minha base de dados 
+
 
 /*vou fazer uma pesquisa no banco de dados
 empregados e mostrar os dados*/
 
-$sql = "SELECT nome, id FROM empregado";
+$sql = "SELECT nome, id FROM empregado ORDER BY id DESC LIMIT 0,6";
 
 $resultado = mysqli_query($conexao, $sql);
 
 if($resultado){
     while($linha = mysqli_fetch_array($resultado)){
-        echo $linha["id"]." - ".$linha["nome"];
+        echo $linha["id"]." - ".$linha["nome"]."<br>";
     }
 }
